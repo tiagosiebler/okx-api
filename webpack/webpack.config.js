efa7c9c194e03516f2251541318ed3c74b6767a1
Tiago Siebler <tiagosiebler@users.noreply.github.com>
2024-12-12 10:06:35 +0000

    chore(): bump build version to node LTS, bump webpack version, update gh action to run lint check

--- webpack/webpack.config.js
+++ webpack/webpack.config.js
@@ -1,6 +1,6 @@
-const webpack = require('webpack');
 const path = require('path');
-const BundleAnalyzerPlugin = require('webpack-bundle-analyzer').BundleAnalyzerPlugin;
+const BundleAnalyzerPlugin =
+  require('webpack-bundle-analyzer').BundleAnalyzerPlugin;
 
 function generateConfig(name) {
   var config = {
@@ -9,28 +9,30 @@ function generateConfig(name) {
       path: path.resolve(__dirname, '../dist'),
       filename: name + '.js',
       sourceMapFilename: name + '.map',
-      library: 'okxapi',
-      libraryTarget: 'umd'
+      library: name,
+      libraryTarget: 'umd',
     },
-    devtool: "source-map",
+    devtool: 'source-map',
     mode: 'production',
 
     resolve: {
       // Add '.ts' and '.tsx' as resolvable extensions.
-      extensions: [".webpack.js", ".web.js", ".ts", ".tsx", ".js"],
+      extensions: ['.webpack.js', '.web.js', '.ts', '.tsx', '.js'],
       alias: {
-        [path.resolve(__dirname, "../lib/util/node-support.js")]:
-          path.resolve(__dirname, "../lib/util/browser-support.js"),
-      }
+        [path.resolve(__dirname, '../lib/util/node-support.js')]: path.resolve(
+          __dirname,
+          '../lib/util/browser-support.js',
+        ),
+      },
     },
 
     module: {
       rules: [
         // All files with a '.ts' or '.tsx' extension will be handled by 'ts-loader'.
-        { test: /\.tsx?$/, loader: "ts-loader" },
+        { test: /\.tsx?$/, loader: 'ts-loader' },
 
         // All output '.js' files will have any sourcemaps re-processed by 'source-map-loader'.
-        { test: /\.js$/, loader: "source-map-loader" },
+        { test: /\.js$/, loader: 'source-map-loader' },
 
         {
           test: /\.m?js$/,
@@ -38,28 +40,30 @@ function generateConfig(name) {
           use: {
             loader: 'babel-loader',
             options: {
-              presets: [['@babel/preset-env', {
-                'targets': {
-                  'node': 'current'
-                }
-              }]]
-            }
-          }
-        }
-      ]
-    }
+              presets: [
+                [
+                  '@babel/preset-env',
+                  {
+                    targets: {
+                      node: 'current',
+                    },
+                  },
+                ],
+              ],
+            },
+          },
+        },
+      ],
+    },
   };
 
   config.plugins = [
-    new webpack.DefinePlugin({
-      'process.env.NODE_ENV': JSON.stringify(process.env.NODE_ENV)
-    }),
     new BundleAnalyzerPlugin({
       defaultSizes: 'stat',
       analyzerMode: 'static',
       reportFilename: '../doc/bundleReport.html',
       openAnalyzer: false,
-    })
+    }),
   ];
 
   return config;
