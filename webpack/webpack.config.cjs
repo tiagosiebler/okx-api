453633db952d5e1c84795860076e2af61eda3e66
JJ-Cro <bryanadams3312@gmail.com>
2026-01-13 14:13:19 +0100

    chore: update webpack configuration to use CommonJS format and adjust pack script in package.json

--- webpack/webpack.config.cjs
+++ webpack/webpack.config.cjs
@@ -4,7 +4,7 @@ const BundleAnalyzerPlugin =
 
 function generateConfig(name) {
   var config = {
-    entry: './lib/index.js',
+    entry: './dist/cjs/index.js',
     output: {
       path: path.resolve(__dirname, '../dist'),
       filename: name + '.js',
@@ -19,9 +19,9 @@ function generateConfig(name) {
       // Add '.ts' and '.tsx' as resolvable extensions.
       extensions: ['.webpack.js', '.web.js', '.ts', '.tsx', '.js'],
       alias: {
-        [path.resolve(__dirname, '../lib/util/node-support.js')]: path.resolve(
+        [path.resolve(__dirname, '../dist/cjs/util/node-support.js')]: path.resolve(
           __dirname,
-          '../lib/util/browser-support.js',
+          '../dist/cjs/util/browser-support.js',
         ),
       },
       fallback: {
@@ -34,31 +34,7 @@ function generateConfig(name) {
 
     module: {
       rules: [
-        // All files with a '.ts' or '.tsx' extension will be handled by 'ts-loader'.
-        { test: /\.tsx?$/, loader: 'ts-loader' },
-
-        // All output '.js' files will have any sourcemaps re-processed by 'source-map-loader'.
-        { test: /\.js$/, loader: 'source-map-loader' },
-
-        {
-          test: /\.m?js$/,
-          exclude: /(node_modules|bower_components|samples|lib|test|coverage)/,
-          use: {
-            loader: 'babel-loader',
-            options: {
-              presets: [
-                [
-                  '@babel/preset-env',
-                  {
-                    targets: {
-                      node: 'current',
-                    },
-                  },
-                ],
-              ],
-            },
-          },
-        },
+        // Code is already transpiled from TypeScript, no additional loaders needed
       ],
     },
   };
